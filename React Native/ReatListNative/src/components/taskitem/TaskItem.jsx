770b5ae7f4f3fb7e10635158e3c763622788744c
GIULIA MARZANO MARTINIANO DOS SANTOS <54781547842@info.local>
2026-08-12 09:08:26 -0300

    B

--- React Native/ReatListNative/src/components/taskitem/TaskItem.jsx
+++ React Native/ReatListNative/src/components/taskitem/TaskItem.jsx
@@ -1,10 +1,26 @@
-import { View, Text } from "react-native"
-import { TaskItemStyle } from "./TaskItemStyle"
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { TaskItemStyle } from "./TaskItemStyle";
 
-export const TaskItem = ({ taskText }) => {
+export const TaskItem = () => {
     return (
         <View style={TaskItemStyle.cardbox}>
-            <Text style={TaskItemStyle.cartText}>{taskText}</Text>
+            <Text style={TaskItemStyle.cardText}>berratriz</Text>
+
+            <View style={TaskItemStyle.buttonGroup}>
+                <TouchableOpacity style={TaskItemStyle.cardButton}>
+                    <Image 
+                        source={require("../../../assets/Lapis.png")}
+                        style={TaskItemStyle.icon}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={TaskItemStyle.cardButton}>
+                    <Image 
+                        source={require("../../../assets/Lixeira.png")}
+                        style={TaskItemStyle.icon}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
-    )
-}
+    );
+};
--- React Native/ReatListNative/src/components/taskitem/TaskItemStyle.js
+++ React Native/ReatListNative/src/components/taskitem/TaskItemStyle.js
@@ -2,30 +2,30 @@ import { StyleSheet } from 'react-native';
 
 export const TaskItemStyle = StyleSheet.create({
   cardbox: {
-    flexDirection: 'row',      
-    alignItems: 'center',      
-    justifyContent: 'space-between', 
-    backgroundColor: '#222a37', 
-    borderRadius: 8,            
-    padding: 16,               
-    marginTop: 12,              
-    width: '100%',
+    flexDirection: "row",           
+    height: 70,
+    marginBottom: 15,
+    backgroundColor: "#313664",
+    alignItems: "center",           
+    justifyContent: "space-between",
+    paddingHorizontal: 15,          
+    borderRadius: 8,                
   },
-  cartText:{
-       color: '#d1d5db',         
-    fontSize: 15,
-    flex: 1,                    
-    marginRight: 10,
-  }, 
-    actionsGroup: {
-    flexDirection: 'row',       
-    gap: 12,                    
+  cardText: {
+    color: "white",
+    fontSize: 16,
+    flex: 1,                       
   },
-  iconImage: {
-    width: 24,                
-    height: 24,               
-    resizeMode: 'contain',     
-  }
+  buttonGroup: {
+    flexDirection: "row",           
+    alignItems: "center",
+  },
+  cardButton: {
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+                
+  },
+ 
 });
-
-
